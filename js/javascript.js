d1e11e2f07461815a3c423465c6e01f9adaa19fc
AbbyAGarcia <agarcia91@hawk.iit.edu>
2025-04-08 20:56:55 -0500

    changed index file, added to js file, created css file

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function innit(){
+  function myEventFunction(){
+    alert('Abby Garcia: This is a test');
+  }
+  var ebutton = document.getElementById('entrybutton');
+  ebutton.addEventListener('click',myEventFunction);
+  
+}
+  
+  
+  
+  
